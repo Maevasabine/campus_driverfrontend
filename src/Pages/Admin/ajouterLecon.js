c0609cbf589daa6f7e425e2b9f8a8e9a84b1a820
Maevasabine <106099296+Maevasabine@users.noreply.github.com>
2026-02-12 08:54:34 -0500

    fix database config

--- src/Pages/Admin/ajouterLecon.js
+++ src/Pages/Admin/ajouterLecon.js
@@ -1,8 +1,18 @@
+import { useState, useEffect } from "react";
 export default function AjouterLecon() {
+  const [section, setSection] = useState([]);
+
+      useEffect(() => {
+      fetch("https://campusdriver-production.up.railway.app/cours/api/section/")
+        .then(res => res.json())
+        .then(data => setSection(data));
+    }, []);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const formData = new FormData(e.target);
+    formData.set("is_preview", e.target.is_preview.checked ? "true" : "false");
 
     await fetch("https://campusdriver-production.up.railway.app/cours/api/creer_lessons/", {
       method: "POST",
@@ -15,7 +25,17 @@ export default function AjouterLecon() {
   return (
     <form onSubmit={handleSubmit}>
       <h2>Ajouter Leçon</h2>
-      <input name="section" placeholder="ID Section" required />
+     <div>
+          <label>Section</label>
+          <select name="section" required>
+            <option value="">-- Aucun --</option>
+            {section.map((sect) => (
+              <option key={sect.id} value={sect.id}>
+                {sect.titre}
+              </option>
+            ))}
+          </select>
+        </div>
       <input name="titre" placeholder="Titre" required />
       <input name="video" type="file" required />
       <label>
--- src/Pages/Admin/ajouterSection.js
+++ src/Pages/Admin/ajouterSection.js
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function AjouterSection() {
+  const [cours, setCours] = useState([]);
   const [form, setForm] = useState({
     cours: "",
     titre: "",
     ordre: 0,
   });
+    useEffect(() => {
+      fetch("https://campusdriver-production.up.railway.app/cours/api/cours/")
+        .then(res => res.json())
+        .then(data => setCours(data));
+    }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,9 +28,26 @@ export default function AjouterSection() {
   return (
     <form onSubmit={handleSubmit}>
       <h2>Ajouter Section</h2>
-      <input name="cours" placeholder="ID Cours" onChange={(e)=>setForm({...form, cours:e.target.value})}/>
-      <input name="titre" placeholder="Titre" onChange={(e)=>setForm({...form, titre:e.target.value})}/>
-      <input name="ordre" type="number" onChange={(e)=>setForm({...form, ordre:e.target.value})}/>
+       <div>
+          <label>Cours</label>
+          <select name="cours" required>
+            <option value="">-- Aucun --</option>
+            {cours.map((cour) => (
+              <option key={cour.id} value={cour.id}>
+                {cour.titre}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Titre </label>
+          <input name="titre" placeholder="Titre" onChange={(e)=>setForm({...form, titre:e.target.value})}/>
+        </div>
+         <div>
+          <label>Ordre </label>
+          <input name="ordre" type="number" onChange={(e)=>setForm({...form, ordre:Number(e.target.value)})}/>
+        </div>
+      
       <button type="submit">Créer</button>
     </form>
   );
